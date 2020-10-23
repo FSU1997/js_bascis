15ce07bd0834f98ab2a022c6b477b8165c47ce9a
Moksleivis <zygimantas.verba@gmail.com>
2020-10-23 15:05:27 +0300

    as

---
+++ js/app.js
@@ -0,0 +1,31 @@
+
+const appName = "JS bascics"; //constant
+
+
+let skaicius;
+skaicius = prompt(message = "sveciu skaicius");
+let svecias;
+console.log("sveciu skaicius:"+skaicius);
+let blacklist;
+blacklist = ['juozas', 'sandra', 'bilas'];
+
+for (let i=0; i<skaicius; i++){
+    if (svecias=blacklist) {
+        console.log("zmogus neileistas");
+        svecias = 0;
+    }
+    else
+    svecias = prompt(message="iveskite varda");
+    console.log(svecias);
+
+
+
+}
+//let svecias = prompt(message="iveskite varda");
+
+
+/*for(let i=0;i<items.length;i++){
+    console.log(i+","+items[i])
+}*/
+
+//console.log("pasirinkimas:"+items[3]);
